44709c20f388d91a4ba896318cb18b20d0c24857
david <david.stankov@haemimont.com>
2022-01-20 17:51:20 +0200

    tree source

--- py/src/ml/test.py
+++ py/src/ml/test.py
@@ -9,8 +9,14 @@ print(df.head())
 df['obese'] = (df.Index > 4).astype('int')
 df.drop('Index', axis=1, inplace=True)
 
+a = df[df['Weight'] > 80]['obese'] == 1
 
-def entropy(X, y):
+print(len(a))
+print(sum(a))
+print(sum(a) / len(a))
+
+
+def entropy(y):
     """
 
     """
@@ -18,11 +24,18 @@ def entropy(X, y):
     p = np.array(list(counter.values())) / len(y)
     return -np.sum(p * np.log2(p))
 
-
-
 weights = df['Weight'].unique()
+data = np.array([0,0])
 for i in range(1,len(weights)):
     weight = weights[i]
     a = df[df['Weight']<weight]
-    e = entropy(a.drop('obese',axis=1),a['obese'])
-    print(e)
+    e = entropy(a['obese'])
+    data = np.vstack([data,[weight,e]])
+
+plt.scatter(data[:,0],data[:,1],s=4)
+#plt.show()
+def inf_gain(y,mask,func=entropy):
+    y_left = y[mask]
+    y_right = y[-mask]
+
+    ig = func(y)-(len(y_left)/len(y)*func(y_left) + len(y_right)/len(y)*func(y_right))
